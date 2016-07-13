51f225245d1532b64a1f79e25f55d6e39b3e746c
Manish kumar <manishchoudhary55@gmail.com>
2016-07-14 03:09:10 +0530

    Added back button

--- app/src/main/java/in/co/techm/ifsc/ui/BankDetailsActivity.java
+++ app/src/main/java/in/co/techm/ifsc/ui/BankDetailsActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -32,6 +33,15 @@ public class BankDetailsActivity extends AppCompatActivity implements View.OnCli
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
         mContext = this;
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
