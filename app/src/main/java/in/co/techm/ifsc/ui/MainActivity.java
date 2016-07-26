118fab98d002e8c5503e893887f888adcad2dd0a
Manish kumar <manishchoudhary55@gmail.com>
2016-07-27 00:10:44 +0530

    DrawerLayout ui changes

--- app/src/main/java/in/co/techm/ifsc/ui/MainActivity.java
+++ app/src/main/java/in/co/techm/ifsc/ui/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
